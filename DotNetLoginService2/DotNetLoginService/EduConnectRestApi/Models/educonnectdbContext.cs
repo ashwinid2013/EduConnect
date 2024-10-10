027ca844ec24cb6568000ed22306fcc25f3eb80a
Aishwarya <aishwaryabadgujar9168@gmail.com>
2024-10-10 08:12:12 +0530

    Updated files

--- DotNetLoginService2/DotNetLoginService/EduConnectRestApi/Models/educonnectdbContext.cs
+++ DotNetLoginService2/DotNetLoginService/EduConnectRestApi/Models/educonnectdbContext.cs
@@ -35,7 +35,7 @@ namespace EduConnectRestApi.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=root;database=educonnectdb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=261803;database=educonnectdb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
             }
         }
 
--- DotNetLoginService2/DotNetLoginService/EduConnectRestApi/obj/Debug/net8.0/EduConnectRestApi.AssemblyInfo.cs
+++ DotNetLoginService2/DotNetLoginService/EduConnectRestApi/obj/Debug/net8.0/EduConnectRestApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduConnectRestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4bf7a32a2bf02cc74a11565413fc71f5c8f209")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduConnectRestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduConnectRestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
